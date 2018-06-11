d4df5cf51c664cca444743d8f8adec2d5124aa65
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-11 11:38:08 -0500

    Adding some basic POCOs to the project - Talks and Registrants

---
+++ DubuqueCodeCampScheduler/DataRetrieval/Models/Registrants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DubuqueCodeCampScheduler
+{
+    public class Registrants
+    {
+        public Guid ID { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+    }
+}
---
+++ DubuqueCodeCampScheduler/DataRetrieval/Models/Talks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DubuqueCodeCampScheduler
+{
+    public class Talks
+    {
+        public Guid ID { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Summary { get; private set; }
+
+        public string DateGiven { get; private set; }
+    }
+}
