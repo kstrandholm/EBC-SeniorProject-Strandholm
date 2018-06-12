47fe17aa2a7f82acfea59d7939d0fb24f571e296
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-12 11:32:19 -0500

    Adding a new POCO for the available sessions

---
+++ DubuqueCodeCampScheduler/DataRetrieval/Models/Sessions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubuqueCodeCampScheduler
+{
+    public class Sessions
+    {
+        public int ID { get; private set; }
+
+        public DateTime SessionStart { get; private set; }
+
+        public DateTime SessionEnd { get; private set; }
+
+       public int LengthInMinutes { get; private set; }
+    }
+}
