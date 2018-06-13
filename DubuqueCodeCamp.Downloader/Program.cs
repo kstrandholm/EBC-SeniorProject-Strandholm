d68eb67825c56de7e9ba89a21aeb8bb2e9249bc4
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-13 13:03:34 -0500

    Got my Downloader program back to normal, and included the FileParser class again, with the related Nuget package - also re-included the RegistrantInformation class

--- DubuqueCodeCamp.Downloader/Program.cs
+++ DubuqueCodeCamp.Downloader/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DubuqueCodeCamp.Downloader
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var registrants = new List<RegistrantInformation>();
+
+            var localFileLocation = ConfigurationManager.AppSettings["LocalFileLocation"];
+            var fileName = "SampleFile.txt";
+
+            try
+            {
+                Console.WriteLine("Getting File " + fileName + "...\n");
+
+                // Download the file
+                SFTPDownload.DownloadFileUsingSftpClient(fileName, localFileLocation);
+
+                // If it suceeded, write so to the console
+                // TODO: Since this will hopefully become an automated process, find some better way to indicate a success vs. failure
+                Console.WriteLine("\nFile retrieved.");
+            }
+            catch (Exception ex)
+            {
+                // Something went wrong - indicate so
+                // TODO: Since this will hopefully become an automated process, find some better way to indicate a success vs. failure
+                Console.WriteLine(
+                    "\nOh no, something went wrong with the file download!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nHere's what I know:");
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+            try
+            {
+                registrants = FileParser.ParseFile(localFileLocation + fileName).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(
+                    "\nOh no, something went wrong with parsing the file!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nHere's what I know:");
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+#if DEBUG
+            Console.WriteLine("\nPress any key to Continue...");
+            Console.ReadKey();
+#endif
         }
     }
 }
---
+++ DubuqueCodeCamp.Downloader/RegistrantInformation.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DubuqueCodeCamp.Downloader
+{
+    public class RegistrantInformation
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public bool IsSpeaker { get; set; }
+
+    }
+}
