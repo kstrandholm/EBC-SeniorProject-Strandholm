f96862e9f421d74b57b946a30631ae4956b51eb9
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-13 09:47:23 -0500

    Considering a good way to indicate what talks the registrant is interested in - the parser can handle this just fine, but I'm not sure how I want to throw in all the information for talk interest into the download file...

---
+++ DCCRegistrantFileDownload/TalkInterest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubuqueCodeCampScheduler
+{
+    public class TalkInterest
+    {
+        public int ID { get; private set; }
+
+        public Talk TalkID { get; private set; }
+
+        public Registrant InterestedRegistrantID { get; private set; }
+    }
+}
