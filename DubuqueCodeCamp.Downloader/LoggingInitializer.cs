2e85ff2e66831528658c6f4cd685e0fd991317cc
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-14 11:53:40 -0500

    Added a Serilogger enricher to get environment details from the machine the code is running on.

--- DubuqueCodeCamp.Downloader/LoggingInitializer.cs
+++ DubuqueCodeCamp.Downloader/LoggingInitializer.cs
@@ -5,13 +5,13 @@ using Serilog.Sinks.MSSqlServer;
 using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
-using System.Runtime.CompilerServices;
+using Serilog.Core;
 
 namespace DubuqueCodeCamp.Downloader
 {
     public class LoggingInitializer
     {
-        public static ILogger InitializeLogger()
+        public static Logger GetLogger()
         {
             var databaseConnectionString = ConfigurationManager.ConnectionStrings["DCCDatabase"].ConnectionString;
             var rollingFileLocation = ConfigurationManager.AppSettings["LoggingFileLocation"];
@@ -24,22 +24,23 @@ namespace DubuqueCodeCamp.Downloader
                     StandardColumn.Level,
                     StandardColumn.Message,
                     StandardColumn.Exception,
+                    StandardColumn.Properties
                 },
                 AdditionalDataColumns = new Collection<DataColumn>
                 {
                     new DataColumn {DataType = typeof(string), ColumnName = "Table", AllowDBNull = true},
                     new DataColumn {DataType = typeof(string), ColumnName = "MachineName", AllowDBNull = true},
-                    new DataColumn {DataType = typeof(string), ColumnName = "StackTrace", AllowDBNull = false},
-                    new DataColumn {DataType = typeof(CallSite), ColumnName = "CallSite", AllowDBNull = false},
-                    new DataColumn {DataType = typeof(string), ColumnName = "Logger", AllowDBNull = false}
+                    new DataColumn {DataType = typeof(string), ColumnName = "SourceContext", AllowDBNull = true}
                 },
                 Id = { ColumnName = "ID" },
                 TimeStamp = { ColumnName = "Date" }
             };
             
             return new LoggerConfiguration()
-                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
                 .WriteTo.MSSqlServer(databaseConnectionString, "Log", LogEventLevel.Information, columnOptions: columnOptions)
+                .WriteTo.Console()
                 .WriteTo.RollingFile(rollingFileLocation)
                 .CreateLogger();
         }
