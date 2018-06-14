6eb0387185c8854dfb3a336c7dcaaa15e1ee0224
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-14 14:56:03 -0500

    Attempting to figure out how to get the equality overrides to work with little success (except for the .Distinct() which does work)

---
+++ DubuqueCodeCamp.DatabaseConnection/IRegistrant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DubuqueCodeCamp.DatabaseConnection
+{
+    public interface IRegistrant
+    {
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string City { get; set; }
+
+        string State { get; set; }
+    }
+}
