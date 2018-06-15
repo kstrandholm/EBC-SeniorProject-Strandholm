749e3364b664e10bded0108bff32d06b0de0f21b
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-15 08:26:01 -0500

    Adding a new class to come up with the actual proposed schedule

---
+++ DubuqueCodeCamp.Scheduler/DetermineSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DubuqueCodeCamp.DatabaseConnection;
+
+namespace DubuqueCodeCamp.Scheduler
+{
+    public class DetermineSchedule
+    {
+        private DCCKellyDatabase _database = new DCCKellyDatabase();
+
+        public void GetProposedSchedule()
+        {
+            List<(int talk, int interestAmount)> interestCount = _database.Talks.Select(talk => new (int talk, int interestAmount){(talk.ID, _database.TalkInterest.Count(interest => interest.TalkID == talk.ID))}).ToList();
+        }
+    }
+}
