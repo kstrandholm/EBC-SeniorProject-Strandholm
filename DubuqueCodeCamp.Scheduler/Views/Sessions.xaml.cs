c11c71662a05f5baceded4f55f3281c2d210b247
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-18 14:11:15 -0500

    Moved the Sessions part of the mainWindow into another View, so that I can switch it between the main Sessions content and a new content I will add soon

---
+++ DubuqueCodeCamp.Scheduler/Views/Sessions.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DubuqueCodeCamp.Scheduler.Views
+{
+    /// <summary>
+    /// Interaction logic for Sessions
+    /// </summary>
+    public partial class Sessions : UserControl
+    {
+        public Sessions()
+        {
+            InitializeComponent();
+        }
+    }
+}
