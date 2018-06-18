c2ed98dd0394aaee7e8b3a56b5b56a45f9655f83
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-18 11:15:20 -0500

    Hooking up Unity and Prism

---
+++ DubuqueCodeCamp.Scheduler/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace DubuqueCodeCamp.Scheduler
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
--- DubuqueCodeCamp.Scheduler/ViewModel/MainWindowViewModel.cs
+++ DubuqueCodeCamp.Scheduler/ViewModel/MainWindowViewModel.cs
@@ -1,10 +1,51 @@
-﻿using PropertyChanged;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using DubuqueCodeCamp.DatabaseConnection;
+using Prism.Commands;
+using Prism.Mvvm;
+using PropertyChanged;
 
-namespace DubuqueCodeCamp.Scheduler.ViewModel
+namespace DubuqueCodeCamp.Scheduler
 {
     [AddINotifyPropertyChangedInterface]
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        private List<ProposedSchedule> _poposedSchedules = new List<ProposedSchedule>
+        {
+            new ProposedSchedule
+            {
+                ID = 5,
+                Room = new Room(),
+                Session = new Session(),
+                Talk = new Talk(),
+                DiagnosticInformation = "Testing",
+                UpdateTime = DateTime.Now
+            }
+        };
 
+        public List<ProposedSchedule> ProposedSchedules
+        {
+            get { return _poposedSchedules.ToList(); }
+            set { SetProperty(ref _poposedSchedules, value); }
+        }
+
+        public ICommand UpdateCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            UpdateCommand = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => ProposedSchedules);
+        }
+
+        private bool CanExecute()
+        {
+            return ProposedSchedules.Any();
+        }
+
+        private void Execute()
+        {
+            ProposedSchedules.First().UpdateTime = DateTime.Now;
+        }
     }
 }
