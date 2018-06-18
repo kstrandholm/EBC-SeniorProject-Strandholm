0e101240d0cbe77c71069877f73e9ed01b3ec2c4
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-18 15:37:01 -0500

    Trying out a Module for view navigation

---
+++ DubuqueCodeCamp.Scheduler/SessionsModule.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Modularity;
+using System;
+
+namespace DubuqueCodeCamp.Scheduler
+{
+    public class SessionsModule : IModule
+    {
+        /// <inheritdoc />
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
