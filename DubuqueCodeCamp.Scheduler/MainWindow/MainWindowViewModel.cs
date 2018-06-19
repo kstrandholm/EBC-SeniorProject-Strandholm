48b5a8536c8d6f72e128a4d56efabc7e51f7ce4d
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-19 15:23:07 -0500

    Implementing CanExecute logic for CreateProposedSchedule and a few comments/cleanup

--- DubuqueCodeCamp.Scheduler/MainWindow/MainWindowViewModel.cs
+++ DubuqueCodeCamp.Scheduler/MainWindow/MainWindowViewModel.cs
@@ -7,15 +7,27 @@ using Prism.Regions;
 
 namespace DubuqueCodeCamp.Scheduler
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// View Model for the MainWindow
+    /// </summary>
     public class MainWindowViewModel : BindableBase
     {
         private readonly IRegionManager _regionManager;
         private readonly IEventAggregator _eventAggregator;
 
+        private bool _canExecuteCreateSchedule;
+
+        public bool CanExecuteCreateSchedule
+        {
+            get => _canExecuteCreateSchedule;
+            set => SetProperty(ref _canExecuteCreateSchedule, value);
+        }
+
         private DateTime _eventDate = DateTime.Today;
         public DateTime EventDate
         {
-            get { return _eventDate; }
+            get => _eventDate;
             set
             {
                 SetProperty(ref _eventDate, value);
@@ -23,23 +35,41 @@ namespace DubuqueCodeCamp.Scheduler
             }
         }
 
-        public DelegateCommand<string> NavigateCommand { get; set; }
-
+        /// <summary>
+        /// If CanExecute returns true creates the Proposed Schedule and raises the ScheduleCreatedEvent
+        /// Otherwise nothing happens
+        /// </summary>
         public ICommand CreateScheduleCommand { get; set; }
 
+        /// <summary>
+        /// Constructor for the MainWindow view model
+        /// </summary>
+        /// <param name="regionManager">Region manager created and passed in by Prism/Unity</param>
+        /// <param name="eventAggregator">Event Aggregator created and passed in by Prism/Unity</param>
         public MainWindowViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
         {
             _regionManager = regionManager;
             _eventAggregator = eventAggregator;
 
             // Define Commands
-            NavigateCommand = new DelegateCommand<string>(Navigate);
             CreateScheduleCommand = new DelegateCommand(Execute, CanExecute);
+
+            // Subscribe to Events
+            _eventAggregator.GetEvent<SessionsUpdatedEvent>().Subscribe(UpdatePropertyCanExecute);
+        }
+
+        /// <summary>
+        /// Update the CanExecuteCreateSchedule to true if it is not already
+        /// Prevents redundant assignments
+        /// </summary>
+        private void UpdatePropertyCanExecute()
+        {
+            CanExecuteCreateSchedule = true;
         }
 
         private bool CanExecute()
         {
-            throw new NotImplementedException();
+            return CanExecuteCreateSchedule;
         }
 
         private void Execute()
