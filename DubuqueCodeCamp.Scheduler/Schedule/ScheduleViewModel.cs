2f95011c83efc75ecfd7caee6b004c184b455dcd
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-19 13:50:50 -0500

    Major changes to Schedule viewModel:  removed the methods that are included in ScheduleOperations and hooking them in instead

--- DubuqueCodeCamp.Scheduler/Schedule/ScheduleViewModel.cs
+++ DubuqueCodeCamp.Scheduler/Schedule/ScheduleViewModel.cs
@@ -1,102 +1,41 @@
-﻿using Prism.Commands;
+﻿using Prism.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
 using DubuqueCodeCamp.DatabaseConnection;
 
 namespace DubuqueCodeCamp.Scheduler
 {
     public class ScheduleViewModel : BindableBase
     {
-        private readonly DCCKellyDatabase _database = new DCCKellyDatabase();
+        private DateTime _eventDate = DateTime.Today;
 
-        private List<ProposedSchedule> _schedules;
-        public List<ProposedSchedule> Scheudles
+        private List<ProposedSchedule> _schedules = ScheduleOperations.GetProposedSchedule(DateTime.Today);
+        public List<ProposedSchedule> Schedules
         {
             get { return _schedules; }
             set { SetProperty(ref _schedules, value); }
         }
 
-        public ScheduleViewModel()
+        public ScheduleViewModel(IEventAggregator eventAggregator)
         {
+            eventAggregator.GetEvent<DateUpdatedEvent>().Subscribe(GetEventDate);
         }
 
-        public List<ProposedSchedule> GetProposedSchedule(DateTime eventDate)
+        private void GetEventDate(DateTime eventDate)
         {
-            // Ensure the Event Date does not have an unnecessary time added
-            eventDate = eventDate.Date;
-
-            return (from sched in _database.ProposedSchedules
-                    join session in _database.Sessions on sched.SessionID equals session.ID
-                    where session.SessionDate == eventDate
-                    select sched).ToList();
+            _eventDate = eventDate;
+            RefreshSchedule();
         }
 
-        public List<Session> GetExistingSessions(DateTime eventDate)
+        private void RefreshSchedule()
         {
-            // Ensure the Event Date does not have an unnecessary time added
-            eventDate = eventDate.Date;
-
-            return (from session in _database.Sessions
-                    where session.SessionDate == eventDate
-                    select session).ToList();
+            Schedules = ScheduleOperations.GetProposedSchedule(_eventDate);
         }
 
-        public void CreateProposedSchedule(DateTime eventDate)
+        private void GetScheduleFullDetail()
         {
-            // Ensure the Event Date does not have an unnecessary time added
-            eventDate = eventDate.Date;
-
-            var cachedTalks = _database.Talks.Select(talk => talk.ID).ToList();
-            var talks = (from talkID in cachedTalks
-                         let count = _database.TalkInterest.Count(interest => interest.TalkID == talkID)
-                         orderby count descending
-                         select talkID).ToList();
-
-            // If there are no sessions, we can't create a proposed schedule
-            if (!_database.Sessions.Any())
-                return;
-
-            var schedulesForDate = (from sched in _database.ProposedSchedules
-                                    join session in _database.Sessions on sched.SessionID equals session.ID
-                                    where session.SessionDate == eventDate
-                                    select sched).ToList();
-
-            // If there are any existing schedules for this date, warn the user that creating a new one will overwrite the existing one
-            if (schedulesForDate.Any())
-            {
-                var result = MessageBox.Show("A proposed schedule already exists. Generating a new one will overwrite the old. Continue?",
-                    "Existing Proposed Schedule", MessageBoxButton.OKCancel);
-                if (result == MessageBoxResult.Cancel)
-                    return;
-            }
-
-            // Combine the Rooms and Sessions to create each unique combination
-            var roomSessions = (from room in _database.Rooms
-                                from session in _database.Sessions
-                                select new { room, session })
-                .OrderByDescending(rs => rs.room.Capacity) // Largest rooms first
-                .ToList();
-
-            // If the number of roomSessions is not equal to the number of talks in interestCount we can't create an accurate schedule
-            if (roomSessions.Count != talks.Count)
-                return;
-
-            for (var i = 0; i < roomSessions.Count; i++)
-            {
-                var roomSession = roomSessions[i];
-                var newSchedule = new ProposedSchedule
-                {
-                    RoomID = roomSession.room.ID,
-                    SessionID = roomSession.session.ID,
-                    TalkID = talks[i],
-                    UpdateTime = DateTime.Now,
-                    DiagnosticInformation = $"Adding Proposed Schedule for event on {eventDate}"
-                };
-                _database.ProposedSchedules.InsertOnSubmit(newSchedule);
-            }
+            // TODO: map the schedule from the database to a new class in here that has the info I need
         }
     }
 }
