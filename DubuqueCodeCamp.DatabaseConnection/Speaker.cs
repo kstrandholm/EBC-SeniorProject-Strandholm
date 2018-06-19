8ccc19bb7a2fdaf5f7a606c1c72bec75b314e245
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-19 14:37:50 -0500

    Introducing Speaker database class which I now have need of

---
+++ DubuqueCodeCamp.DatabaseConnection/Speaker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace DubuqueCodeCamp.DatabaseConnection
+{
+    [Table(Name = "Speakers")]
+    public class Speaker : IRegistrant
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public string ID { get; set; }
+
+        [Column]
+        public string FirstName { get; set; }
+
+        [Column]
+        public string LastName { get; set; }
+
+        [Column(CanBeNull = true)]
+        public string StreetAddress { get; set; }
+
+        [Column]
+        public string City { get; set; }
+
+        [Column]
+        public string State { get; set; }
+
+        [Column(CanBeNull = true)]
+        public string EmailAddress { get; set; }
+
+        [Column]
+        public DateTime UpdateTime { get; set; }
+
+        [Column]
+        public string DiagnosticInformation { get; set; }
+    }
+}
