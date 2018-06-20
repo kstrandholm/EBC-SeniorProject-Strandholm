90d751ec8e391989fec78f622df26533ad27ed15
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-20 09:12:39 -0500

    Creating a Register view and viewModel

---
+++ DubuqueCodeCamp.Registration/Register/RegisterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DubuqueCodeCamp.Registration
+{
+    /// <summary>
+    /// Interaction logic for RegisterView
+    /// </summary>
+    public partial class RegisterView : UserControl
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
