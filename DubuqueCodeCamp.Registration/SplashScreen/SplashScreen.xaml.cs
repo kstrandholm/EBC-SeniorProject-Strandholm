155c50b203de8b820fa6b8ff8953a54b201978f1
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-20 08:45:20 -0500

    Fleshing out the Splash Screen UI and establishing the basics of the viewModel

---
+++ DubuqueCodeCamp.Registration/SplashScreen/SplashScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DubuqueCodeCamp.Registration.SplashScreen
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen
+    /// </summary>
+    public partial class SplashScreen : UserControl
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DubuqueCodeCamp.Registration/SplashScreen/SplashScreenViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Prism.Events;
+using Prism.Regions;
+
+namespace DubuqueCodeCamp.Registration.SplashScreen
+{
+    public class SplashScreenViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        private DelegateCommand<string> NavigateCommand { get; set; }
+
+        public SplashScreenViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            // Define Commands
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string destination)
+        {
+            _regionManager.RequestNavigate("MainRegion", destination);
+        }
+    }
+}
