376ae0d1bff40a1eec11305ab287cb801258ffbd
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-20 13:45:29 -0500

    Create some new events and a new class to contain information on what talks the registrant has selected

---
+++ DubuqueCodeCamp.Registration/Register/ChosenTalk.cs
@@ -0,0 +1,11 @@
+﻿using DubuqueCodeCamp.DatabaseConnection;
+
+namespace DubuqueCodeCamp.Registration
+{
+    public class ChosenTalk
+    {
+        public Talk Talk { get; set; }
+
+        public bool Chosen { get; set; }
+    }
+}
---
+++ DubuqueCodeCamp.Registration/Register/UpdatedTalkInterestsEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Prism.Events;
+
+namespace DubuqueCodeCamp.Registration
+{
+    public class UpdatedTalkInterestsEvent : PubSubEvent<List<ChosenTalk>>
+    {
+    }
+}
