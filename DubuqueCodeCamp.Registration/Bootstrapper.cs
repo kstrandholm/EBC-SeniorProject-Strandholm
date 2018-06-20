7de0db57e10c6aa15272b42d51204f71141e950e
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-20 08:35:20 -0500

    Creating the "shell" of the registration kiosk program and including Prism things that it requires

---
+++ DubuqueCodeCamp.Registration/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace DubuqueCodeCamp.Registration
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        /// <inheritdoc />
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<MainWindow>("MainWindow");
+        }
+
+        /// <inheritdoc />
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+    }
+}
