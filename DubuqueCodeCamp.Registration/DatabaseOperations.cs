dbab72725551c5e3af6e9b14b5429c4cc8355a85
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-20 11:28:53 -0500

    Creating another DatabaseOperations class to handle the, as expected, operations on the database

---
+++ DubuqueCodeCamp.Registration/DatabaseOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DubuqueCodeCamp.DatabaseConnection;
+
+namespace DubuqueCodeCamp.Registration
+{
+    public class DatabaseOperations
+    {
+        private static DCCKellyDatabase _database = new DCCKellyDatabase();
+
+        public static void GetTalks()
+        {
+            var talks = _database.Talks.Where(talk => talk.ID == 7);
+        }
+
+        public static void SaveRegistration()
+        {
+            _database.SubmitChanges();
+        }
+    }
+}
