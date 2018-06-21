d38020cfc240605e2961954f75149fdbd4a12fd8
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-21 09:41:14 -0500

    Adding final comments to the classes in the SHARED project representing tables in the database

--- DubuqueCodeCamp.DatabaseConnection/Speaker.cs
+++ DubuqueCodeCamp.DatabaseConnection/Speaker.cs
@@ -3,33 +3,50 @@ using System.Data.Linq.Mapping;
 
 namespace DubuqueCodeCamp.DatabaseConnection
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Class that represents a record in the Speakers table
+    /// </summary>
     [Table(Name = "Speakers")]
     public class Speaker : IPerson
     {
+        /// <summary>
+        /// ID of the Speaker record
+        /// </summary>
         [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int ID { get; set; }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// First Name of the person represented by this Speaker record
+        /// </summary>
         [Column]
         public string FirstName { get; set; }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Last Name of the person represented by this Speaker record
+        /// </summary>
         [Column]
         public string LastName { get; set; }
 
-        [Column(CanBeNull = true)]
-        public string StreetAddress { get; set; }
-
-        [Column]
-        public string City { get; set; }
-
-        [Column]
-        public string State { get; set; }
-
+        /// <inheritdoc />
+        /// <summary>
+        /// Email Address of the person represented by this Speaker record
+        /// </summary>
+        // TODO: remove nullability once database is taken care of
         [Column(CanBeNull = true)]
         public string EmailAddress { get; set; }
 
+        /// <summary>
+        /// Last update time of this Speaker record
+        /// </summary>
         [Column]
         public DateTime UpdateTime { get; set; }
 
+        /// <summary>
+        /// Latest diagnostic Information for this Speaker record
+        /// </summary>
         [Column]
         public string DiagnosticInformation { get; set; }
     }
--- DubuqueCodeCamp.DatabaseConnection/TalkInterest.cs
+++ DubuqueCodeCamp.DatabaseConnection/TalkInterest.cs
@@ -1,26 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Linq;
 using System.Data.Linq.Mapping;
-using System.Text;
 
 namespace DubuqueCodeCamp.DatabaseConnection
 {
+    /// <summary>
+    /// Class that represents a record in the TalkInterests table
+    /// </summary>
     [Table]
     public class TalkInterest
     {
+        /// <summary>
+        /// ID of the TalkInterest record
+        /// </summary>
         [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int ID { get; set; }
 
+        /// <summary>
+        /// ID of the Talk related to this TalkInterest record
+        /// </summary>
         [Column]
         public int TalkID { get; set; }
 
+        /// <summary>
+        /// ID of the Registrant interested in the talk associated with this TalkInterest record
+        /// </summary>
         [Column]
         public int InterestedRegistrantID { get; set; }
 
+        /// <summary>
+        /// Last update time of this TalkInterest record
+        /// </summary>
         [Column]
         public DateTime UpdateTime { get; set; }
 
+        /// <summary>
+        /// Latest diagnostic Information for this TalkInterest record
+        /// </summary>
         [Column]
         public string DiagnosticInformation { get; set; }
     }
