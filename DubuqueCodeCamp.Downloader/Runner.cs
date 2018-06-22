7bb99f752a0c2b8cd0fca49d52c96928cc4132fa
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-22 11:43:41 -0500

    Using statements

--- DubuqueCodeCamp.Downloader/Runner.cs
+++ DubuqueCodeCamp.Downloader/Runner.cs
@@ -24,45 +24,46 @@ namespace DubuqueCodeCamp.Downloader
 #endif
             var localFileLocation = ConfigurationManager.AppSettings["LocalFileLocation"];
             var fileName = "SampleFile.txt";
-            var logger = LoggingInitializer.GetLogger();
 
-            // Download the file from the FTP site
-            var ftp = new SftpDownload();
-            var messageToUse = ftp.DownloadFileUsingSftpClient(fileName, localFileLocation)
-                ? "\nFile retrieved."
-                : "\nFile not downloaded.";
-            logger.Information(messageToUse, fileName, localFileLocation);
-
-            // Parse the file from the local file path
-            List<RegistrantInformation> registrants;
-            var filePath = localFileLocation + fileName;
-
-            if (File.Exists(filePath))
+            using (var logger = LoggingInitializer.GetLogger())
             {
-                var streamReader = new StreamReader(filePath);
-                var fileParser = new FileParser(streamReader);
+                // Download the file from the FTP site
+                var ftp = new SftpDownload();
+                var messageToUse = ftp.DownloadFileUsingSftpClient(fileName, localFileLocation)
+                    ? "\nFile retrieved."
+                    : "\nFile not downloaded.";
+                logger.Information(messageToUse, fileName, localFileLocation);
 
-                registrants = fileParser.ParseFile();
-            }
-            else
-            {
-                // If the file does not exist, throw an exception
-                throw new FileNotFoundException($"File {fileName} does not exist in {localFileLocation}.", fileName);
-            }
+                // Parse the file from the local file path
+                List<RegistrantInformation> registrants;
+                var filePath = localFileLocation + fileName;
 
-            // If the parser could not get any records, don't try to save to the database
-            if (registrants.Any())
-            {
-                // Write the records to the database
-                var writer = new WriteToDatabase();
-                writer.WriteDownloadRecords(registrants);
-            }
+                if (File.Exists(filePath))
+                {
+                    var streamReader = new StreamReader(filePath);
+                    var fileParser = new FileParser(streamReader);
+
+                    registrants = fileParser.ParseFile();
+                }
+                else
+                {
+                    // If the file does not exist, throw an exception
+                    throw new FileNotFoundException($"File {fileName} does not exist in {localFileLocation}.", fileName);
+                }
+
+                // If the parser could not get any records, don't try to save to the database
+                if (registrants.Any())
+                {
+                    // Write the records to the database
+                    var writer = new WriteToDatabase();
+                    writer.WriteDownloadRecords(registrants);
+                }
 
 #if DEBUG
-            Console.WriteLine("\nPress any key to Continue...");
-            Console.ReadKey();
+                Console.WriteLine("\nPress any key to Continue...");
+                Console.ReadKey();
 #endif
-            logger.Dispose();
+            }
         }
     }
 }
