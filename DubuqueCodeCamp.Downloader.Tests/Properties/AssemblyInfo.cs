eaea5ae352269d6441ccbd41f8153eb09d01fe17
Kelly Strandholm <kstrandholm@pltnm.com>
2018-06-22 08:55:43 -0500

    Beginnings of a test class, but it's not going well, and I suspect I don't actually have my head around testing.  That or I totally wrote my project wrong for unit testing...

---
+++ DubuqueCodeCamp.Downloader.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DubuqueCodeCamp.Downloader.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DubuqueCodeCamp.Downloader.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("777374a2-1eed-4e5e-9258-141aa315b347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DubuqueCodeCamp.Downloader.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using DubuqueCodeCamp.Downloader;
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DubuqueCodeCamp.Downloader.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Test_FileParser_ParsesCorrectlyWrittenFile()
+        {
+            var stringReader =
+                new StringReader(
+                    "Kelly | Strandholm | 'kstrandholm@pltnm.com' | Programmer | 08/15/1995 | 8 | 9 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 18 | 10 | 11 | 12 | 13 | 14 | 15 | 16");
+
+            FileParser.ParseFile();
+        }
+    }
+}
